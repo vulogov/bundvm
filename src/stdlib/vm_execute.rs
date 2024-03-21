0db24c8d61660f8b87f3a66a4801a1458921cfc7
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-03-20 21:02:58 -0600

    Dev #1

--- src/stdlib/vm_execute.rs
+++ src/stdlib/vm_execute.rs
@@ -1,4 +1,10 @@
+extern crate log;
 
-pub fn instructions(_inst: String) {
-
+pub fn instructions(inst: String) {
+    if inst.len() == 0 {
+        log::error!("No instructions has been passed");
+    }
+    for i in inst.lines() {
+        log::debug!("Instruction received. len={}", &i.len());
+    }
 }
