6d30e70b46f8f1190c6d068deeb7abdae0395b0b
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-03-27 13:54:25 -0600

    Dev #13

--- src/vm/vm_callstack.rs
+++ src/vm/vm_callstack.rs
@@ -1,10 +1,9 @@
 extern crate log;
 use crate::vm::BUNDCore;
-use easy_error::{Error};
 
 impl BUNDCore {
-    pub fn is_callstack_nonempty(&mut self) -> Result<bool, Error> {
-        Ok(self.call_stack.len() > 0)
+    pub fn is_callstack_nonempty(&mut self) -> bool {
+        self.call_stack.len() > 0
     }
 
 }
--- src/vm_stdlib/stdlib_terminate_and_execute.rs
+++ src/vm_stdlib/stdlib_terminate_and_execute.rs
@@ -3,10 +3,36 @@ use std::collections::VecDeque;
 use easy_error::{bail, ensure, Error};
 use crate::vm::BUNDCore;
 use rust_dynamic::value::{Value};
-use rust_dynamic::types::{LIST,FIFO,QUEUE,LAMBDA, NONE};
+use rust_dynamic::types::{LIST,FIFO,QUEUE,LAMBDA, NONE, CALL};
 use crate::vm::vm_applicatives::{BundApplicative, NOEXTRA};
 
 fn stdlib_terminate_and_execute_fun(ctx: &mut BUNDCore, _name: &str, _value: Value) -> Result<Option<Value>, Error> {
+    if ctx.is_callstack_nonempty() {
+        log::debug!("Elements has been detrected in callstack, execute them first");
+        match ctx.call_stack.pop_back() {
+            Some(value) => {
+                ensure!(value.dt == CALL, "Invalid JSON type for instruction");
+                match value.cast_string() {
+                    Ok(f_call) => {
+                        match ctx.call(f_call) {
+                            Ok(_) => {
+                                return Ok(None);
+                            }
+                            Err(err) => {
+                                bail!("{}", err);
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        bail!("{}", err);
+                    }
+                }
+            }
+            None => {
+                bail!("Something fishy, call stack is empty or not!");
+            }
+        }
+    }
     ensure!(ctx.stack.stack_len() > 0, "VM: stack depth is not deep enough for that operation");
     let mut scaffold: VecDeque<Value> = VecDeque::new();
     loop {
