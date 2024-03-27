abcbfd930e0bfdbc9853010cabb0a98d1c468251
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-03-27 14:09:59 -0600

    Dev #14

--- src/vm/vm_call.rs
+++ src/vm/vm_call.rs
@@ -1,10 +1,72 @@
 extern crate log;
 use crate::vm::BUNDCore;
 use crate::vm::vm_applicatives::{NOEXTRA, JUSTONE, JUSTTWO, TAKEALL};
+use rust_dynamic::types::{NONE};
 use rust_dynamic::value::{Value};
 use easy_error::{bail, ensure, Error};
 
 impl BUNDCore {
+    pub fn stack_take_one(&mut self) -> Result<Value, Error> {
+        let mut res = Value::list();
+        match self.stack.pull() {
+            Some(value) => {
+                match value.dt {
+                    NONE => {
+                        bail!("Separator found during stack reading");
+                    }
+                    _ => {
+                        res.push(value);
+                    }
+                }
+            }
+            None => {
+                bail!("Stack is not deep enough for the call requested");
+            }
+        }
+        return Ok(res);
+    }
+    pub fn stack_take_two(&mut self) -> Result<Value, Error> {
+        let mut res = Value::list();
+        for _ in 0..2 {
+            match self.stack.pull() {
+                Some(value) => {
+                    match value.dt {
+                        NONE => {
+                            bail!("Separator found during stack reading");
+                        }
+                        _ => {
+                            res.push(value);
+                        }
+                    }
+                }
+                None => {
+                    bail!("Stack is not deep enough for the call requested");
+                }
+            }
+        }
+        return Ok(res);
+    }
+    pub fn stack_take_all(&mut self) -> Result<Value, Error> {
+        let mut res = Value::list();
+        loop {
+            match self.stack.pull() {
+                Some(value) => {
+                    match value.dt {
+                        NONE => {
+                            bail!("Separator found during stack reading");
+                        }
+                        _ => {
+                            res.push(value);
+                        }
+                    }
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+        return Ok(res);
+    }
     pub fn call(&mut self, fun: String) -> Result<(), Error> {
         log::debug!("BUND VM: calling functor: {}", &fun);
         if self.have_applicative(&fun.clone()) {
@@ -51,14 +113,72 @@ impl BUNDCore {
                     }
                     JUSTONE => {
                         ensure!(self.stack.stack_len() >= 1, "VM: Stack is not deep enough for {} call. len()=1 required", &fun);
-
+                        match self.stack_take_one() {
+                            Ok(param) => {
+                                match (app.f)(self, &app.name, param) {
+                                    Ok(res) => {
+                                        match res {
+                                            Some(value) => {
+                                                self.stack.push(value);
+                                            }
+                                            None => {},
+                                        }
+                                    }
+                                    Err(err) => {
+                                        return Err(err);
+                                    }
+                                }
+                            }
+                            Err(err) => {
+                                bail!("{}", err);
+                            }
+                        }
                     }
                     JUSTTWO => {
                         ensure!(self.stack.stack_len() >= 2, "VM: Stack is not deep enough for {} call. len()=2 required", &fun);
+                        match self.stack_take_two() {
+                            Ok(param) => {
+                                match (app.f)(self, &app.name, param) {
+                                    Ok(res) => {
+                                        match res {
+                                            Some(value) => {
+                                                self.stack.push(value);
+                                            }
+                                            None => {},
+                                        }
+                                    }
+                                    Err(err) => {
+                                        return Err(err);
+                                    }
+                                }
+                            }
+                            Err(err) => {
+                                bail!("{}", err);
+                            }
+                        }
                     }
                     TAKEALL => {
                         ensure!(self.stack.stack_len() > 0, "VM: Stack is not deep enough for {} call", &fun);
-
+                        match self.stack_take_all() {
+                            Ok(param) => {
+                                match (app.f)(self, &app.name, param) {
+                                    Ok(res) => {
+                                        match res {
+                                            Some(value) => {
+                                                self.stack.push(value);
+                                            }
+                                            None => {},
+                                        }
+                                    }
+                                    Err(err) => {
+                                        return Err(err);
+                                    }
+                                }
+                            }
+                            Err(err) => {
+                                bail!("{}", err);
+                            }
+                        }
                     }
                     _ => bail!("VM: illegal operant for applicative"),
                 };
