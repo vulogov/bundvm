b25d05fbd7846ef55da881f1405f6c47ce7c8bae
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-04-12 13:59:45 -0600

    Dev #32

--- src/vm_stdlib/stdlib_execute.rs
+++ src/vm_stdlib/stdlib_execute.rs
@@ -6,35 +6,45 @@ use rust_dynamic::value::{Value};
 use crate::vm::vm_applicatives::{BundApplicative, JUSTONE};
 
 fn stdlib_execute_fun(ctx: &mut BUNDCore, _name: &str, value: Value) -> Result<Option<Value>, Error> {
-    log::debug!("Executing element on the stack: {:?}", &value);
-    match value.dt {
-        types::PTR => {
-            match value.cast_string() {
-                Ok(ptr_name) => {
-                    log::debug!("Calling ctx.call({})", &ptr_name);
-                    match ctx.call(ptr_name) {
-                        Ok(_) => {
-                            return Ok(None);
+    match value.cast_list() {
+        Ok(args) => {
+            for arg in args {
+                log::debug!("Executing element on the stack: {:?}", &arg);
+                match arg.dt {
+                    types::PTR => {
+                        match arg.cast_string() {
+                            Ok(ptr_name) => {
+                                log::debug!("Calling ctx.call({})", &ptr_name);
+                                match ctx.call(ptr_name) {
+                                    Ok(_) => {
+                                        return Ok(None);
+                                    }
+                                    Err(err) => {
+                                        bail!("{}", err);
+                                    }
+                                }
+                            }
+                            Err(err) => {
+                                bail!("{}", err);
+                            }
                         }
-                        Err(err) => {
-                            bail!("{}", err);
+                    }
+                    _ => {
+                        match ctx.vm_route(value) {
+                            Ok(_) => {
+                                return Ok(None);
+                            }
+                            Err(err) => {
+                                bail!("{}", err);
+                            }
                         }
                     }
                 }
-                Err(err) => {
-                    bail!("{}", err);
-                }
             }
+            return Ok(None);
         }
-        _ => {
-            match ctx.vm_route(value) {
-                Ok(_) => {
-                    return Ok(None);
-                }
-                Err(err) => {
-                    bail!("{}", err);
-                }
-            }
+        Err(err) => {
+            bail!("{}", err);
         }
     }
 }
